88efaf9a2606070fe0ac57b4c7fa320d3293caf6
Lam Pham <opro1801@gmail.com>
2022-01-07 15:30:43 +0800

    fetching question data

--- components/QuestionContent.tsx
+++ components/QuestionContent.tsx
@@ -1,6 +1,9 @@
+import { useQuery } from '@apollo/client';
 import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { GET_QUESTION_DETAIL_QUERY } from '../apiService/questionBank/questionBankGQL';
 import { useQuestionContext } from '../contexts/QuestionContext';
+import { Loading } from './Loading';
 
 interface QuestionContentInterface {
   //   questionId: string;
@@ -11,7 +14,15 @@ const QuestionContent: React.FC<QuestionContentInterface> = (
     //   questionId,
   },
 ) => {
-  const { totalQuestions } = useQuestionContext();
+  const { totalQuestions, questions, currentQuestion } = useQuestionContext();
+  const { data, loading, error } = useQuery(GET_QUESTION_DETAIL_QUERY, {
+    variables: { id: questions[currentQuestion].id },
+  });
+  if (loading) return <Loading />;
+  //   console.log(data.question.content);
+  const answers = data.question.content.answers;
+  const questionBody = data.question.content.body;
+  console.log(questionBody);
   return <View></View>;
 };
 
--- contexts/QuestionContext.tsx
+++ contexts/QuestionContext.tsx
@@ -3,17 +3,32 @@ import React, { useState, useEffect, FC, useContext, Dispatch } from 'react';
 import { Platform, Dimensions } from 'react-native';
 import { GET_QUESTIONS_QUERY } from '../apiService/questionBank/questionBankGQL';
 import { Loading } from '../components/Loading';
+import { ProgressStatus } from '../components/ProgressIndicator';
 
 interface QuestionContextInterface {
   totalQuestions: number;
   currentQuestion: number;
   setQuestion: (value: number) => void;
+  statusQuestions: QuestionInterface[];
+  questions: InitialQuestionInterface[];
+}
+
+interface QuestionInterface {
+  id: string;
+  status: ProgressStatus;
+}
+
+interface InitialQuestionInterface {
+  id: string;
+  _typename: string;
 }
 
 const defaultState = {
   totalQuestions: 0,
   currentQuestion: 0,
   setQuestion: (value: number) => {},
+  statusQuestions: [],
+  questions: [],
 };
 
 export const QuestionContext =
@@ -21,20 +36,33 @@ export const QuestionContext =
 
 export const QuestionContextProvider: FC = ({ children }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  //   const [questions, setQuestions] = useState<QuestionInterface[]>([]);
   const setQuestion = (value: number) => {
     setQuestion(value);
   };
+  //   useEffect(() => {
+  //       if(data != undefined)
+  //     setQuestions(
+  //       data.questions
+  //         .slice(0, 6)
+  //         .map((_item: { id: string; _typename: string }) => {
+  //           return { id: _item.id, status: ProgressStatus.INACTIVE };
+  //         }),
+  //     );
+  //   }, [data]);
   const { data, error, loading } = useQuery(GET_QUESTIONS_QUERY);
   if (loading) return <Loading />;
   const questions = data.questions.slice(0, 6);
   const totalQuestions = questions.length;
-  console.log('total questions: ', totalQuestions);
+  //   console.log(questions);
   return (
     <QuestionContext.Provider
       value={{
         totalQuestions,
         currentQuestion,
         setQuestion,
+        statusQuestions: [],
+        questions,
       }}
     >
       {children}
